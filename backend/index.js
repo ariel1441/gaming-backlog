6ba235fb418a21dc5a175b170d3e2d8a7f8a28a6
ariel ben akiva <arielbenakiva3007@gmail.com>
2025-08-05 13:39:10 +0300

    added small Security methods like helemt json data transfer limit and compression for a small performanc boost

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,18 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import helmet from 'helmet';
 import gamesRouter, { initCache } from './routes/games.js';
 import authRouter from './routes/auth.js';
 import corsOptions from './config/cors.js';
+import compression from 'compression';
 
 const app = express();
+
+app.use(helmet());
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
+app.use(compression());
 console.log(`starting`);
 
 // Load RAWG cache from disk
@@ -21,5 +26,5 @@ app.use('/api/auth', authRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
